5ff7df1a37bb1ba3023b2deae8fed957838347d6
pilarRomero <pily_romero@yahoo.com.mx>
2024-02-28 23:11:11 -0600

    11 errores

--- miTienda/src/components/ItemListContainer/ItemListContainer.jsx
+++ miTienda/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,24 +1,25 @@
 import { useEffect, useState } from "react";
 import "./styles.css"
-import { getFirestore, doc, getDoc, getDocs, collection } from "firebase/firestore"; 
+import { getFirestore, doc, getDoc, getDocs, collection } from "firebase/firestore";
 import { ItemList } from "../ItemList/ItemList";
 
 
 
 
 const ItemListContainer = () => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   useEffect(() => {
-    const db = getFirestore ();
-    const productoRef = collection (db, "productos", "03GAnsrvWyQ2n8XebQPr" );
-  getDocs(productoRef).then(collection => {
-    if(documento.exists()){
-      console.log(collection.docs())
-    }
-  }) 
+    const db = getFirestore();
+    const productoRef = collection(db, "productos");
+    getDocs(productoRef).then(collection => {
+      const productos = collection.docs.map((doc) => {
+        return doc;
+      })
+
+      console.log();
+    });
   }, []);
-  return <ItemList products ={products} />;
-  
-        };
+  return <ItemList products={products} />;
+};
 
 export default ItemListContainer
